600a159a771d3383789fcf63c52a562fa2e58c3b
Laurent Bossavit <laurent.git@bossavit.com>
2018-03-06 15:50:24 +0100

    Bump to Babel 7 and rebuild

--- test/fixtures/i18n_js/valid.js
+++ test/fixtures/i18n_js/valid.js
@@ -1,4 +1,14 @@
+// Are we supporting the latest in syntax trends ? Let's use an unrelated test to find out.
 @decorator
-class Unused {}
+class Unused {
+	render() {
+		return (
+			<>
+				<div />
+				<div />
+			</>
+		);
+	}
+}
 
-I18n.t('welcome, %{name}', {name: name});
+I18n.t("welcome, %{name}", { name: name });
